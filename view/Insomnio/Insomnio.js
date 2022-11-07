f4fbcff3663e6dba434459fa84cf68ba000f319c
Yomcarthur <116308688+Yomcarthur@users.noreply.github.com>
2022-11-06 20:16:08 -0500

    Add files via upload

---
+++ view/Insomnio/Insomnio.js
@@ -0,0 +1,24 @@
+
+function resultado() {
+    let sumaTotal = 0;
+    let contadorPreg = 0;
+    var contador = 0;
+    let respPregunta = new Array();
+    let respUser = new Array();
+    for (let i = 0; i < 7 ; i++) {
+        respPregunta = document.getElementsByName('p' + (i + 1));
+        for (let j = 0; j <= 4; j++) {
+            if (respPregunta[j].checked == true) {
+
+                //contadorPreg++;
+                //respUser[i] = respPregunta[j].value;
+                contador = contador + parseInt(respPregunta[j].value);
+
+            }
+        }
+    }
+
+
+
+    document.getElementById('result').innerHTML = contador;
+}
